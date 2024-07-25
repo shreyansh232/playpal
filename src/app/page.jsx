cc44ac291cd6663b82eb3f4d13459502b15a64bb
Shreyansh Pandey <shreyansh487@gmail.com>
2024-07-25 13:55:15 +0530

    feat: add header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Footer from "@/components/Home/Footer";
+import Header from "@/components/Home/Header";
 
 export default function Home() {
   return (
     <>
+    <Header />
     <h1 className="text-5xl text-center mt-10">Home</h1>
+    <Footer />
     </>
   );
 }
